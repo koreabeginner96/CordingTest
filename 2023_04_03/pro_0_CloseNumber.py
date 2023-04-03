957d136929769d9facf775a30f506d59dd75dc4a
Back_end_Developer_SH <zx028906@naver.com>
2023-04-03 18:10:03 +0900

    문제의 핵심은 permutations 와 .join(list)다. 순열 과 join을 잘 이용하자

---
+++ 2023_04_03/pro_0_CloseNumber.py
@@ -0,0 +1,15 @@
+def solution(array:list, n):
+    B=[]
+    array.append(n)
+    array.sort()
+    C=array.index(n)
+    B.append(array[C-1])
+    if array[-1] ==n:
+        return array[C-1]
+    B.append(array[C+1])
+    if n-B[0]>B[1]-n:
+        return B[1]
+    else:
+        return B[0]
+print(solution([3,10,28],20))
+
