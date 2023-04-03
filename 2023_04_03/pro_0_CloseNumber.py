1e325dfc2f162fcf010ea0fc3a31a31b6e0d275c
Back_end_Developer_SH <zx028906@naver.com>
2023-04-03 18:39:52 +0900

    sorted(arr, key=lambda x : x) arr=x를 지정할 전체 단위 맨 뒤 x : 앞서반복할 단위를 어떤식으로 정렬하지 cf x[0]인덱스 ,x는 사전정렬
    abs() 절대 함수
    
    abs 와 람다솔트를 알면 쉽게 풀수있다.

--- 2023_04_03/pro_0_CloseNumber.py
+++ 2023_04_03/pro_0_CloseNumber.py
@@ -1,10 +1,4 @@
-'''
-arr=['abc', 'bac','bca']
-sorted(arr, key=lambda x : x)
-arr=x를 지정할 전체 단위
-맨 뒤 x : 앞서반복할 단위를 어떤식으로 정렬하지
-cf x[0]인덱스 ,x는 사전정렬
-'''
+
 def solution(array:list, n):
     B=[]
     array.append(n)
