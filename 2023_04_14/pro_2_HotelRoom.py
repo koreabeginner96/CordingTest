ff27e34d15894d91de6fece569f9b9ac69b2cb04
Back_end_Developer_SH <zx028906@naver.com>
2023-04-14 14:49:03 +0900

    for i in range(x,y):  if dict.get(i) == None:   dict[i]=1  else:   dict[i]+=1     return max(dict.values()) 범위설정해서 교차되면 값을 올려주는 방법을 구현해야한다. 그래도 시간나왔을때 분으로 바꿔주는 건 잘했다.

---
+++ 2023_04_14/pro_2_HotelRoom.py
@@ -0,0 +1,19 @@
+def time2val(time):
+   a,b= map(int,time.split(':'))
+   answer= a*60+b
+   return answer
+def solution(book_time):
+    dict={}
+    for x,y in book_time:
+        x=time2val(x)
+        y=time2val(y)+10
+        for i in range(x,y):
+            if dict.get(i) == None:
+                dict[i]=1
+            else:
+                dict[i]+=1
+    return max(dict.values())
+
+
+
+    return
