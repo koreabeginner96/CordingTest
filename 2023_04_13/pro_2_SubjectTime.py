27565307114a9e7fc5ce32162f96d17b34771bfe
Back_end_Developer_SH <zx028906@naver.com>
2023-04-14 12:07:26 +0900

    A,B=map(int,start.strip(':')) stack.sort(key=lambda x : x[1]) time,cnt=stack[0][1],0 cnt+=1 time+=1
    1)스택사용
    2)정렬 plan.sort(key=lambda x : x[1])
    3)시간정리-> 시를 분으로 나오는건 무조건 분으로 전부 처리해야한다.
    4)dict로 정리를 하자 변수[시간]:이름,걸리는 시간
    5)시작시간정렬 첫번째로 정할 것

--- 2023_04_13/pro_2_SubjectTime.py
+++ 2023_04_13/pro_2_SubjectTime.py
@@ -1,31 +1,43 @@
-def solution(plans):
-    answer = []
-    plan = []
-    stack = []
-    length = len(plans)
-    info = dict()
-    for name, start, playtime in plans:
-        h, m, = map(int, start.split(':'))
-        playtime = int(playtime)
-        start = h*60 + m    # 분으로 변환
-        info[start] = [name, playtime]    # dict에 시간에 따른 정보 저장
-        plan.append([name, start, playtime])
+import collections
 
-    plan.sort(key=lambda x: x[1])    # 과제 시작 시간으로 정렬
 
-    time, cnt = plan[0][1], 0    # 현재 시간, 완료한 과제 수
+def solution(picks, minerals):
+    answer = 25 * 50
+    names = {"diamond": 0, "iron": 1, "stone": 2}
+    fatigues = [[1, 1, 1], [5, 1, 1], [25, 5, 1]]
 
-    while cnt < length:
-        if stack:    # 현재 하고 있는 과제가 존재하는경우
-            stack[-1][1] -= 1    # 가장 먼저 해야하는 과제 1분 과제 수행
-            if stack[-1][1] == 0:    # 가장 먼저 해야하는 과제를 완료했다면 answer에 추가하고 stack에서 제거
-                answer.append(stack[-1][0])
-                stack.pop()
-                cnt += 1    # 완료한 과제 카운트
+    queue = collections.deque()
+    # (남은 곡괭이 수 array, 현재 곡괭이, 현재 곡괭이 잔여 횟수, 현재 광물, 현재까지 피로도)
+    queue.append((picks, -1, 0, 0, 0))
 
-        if time in info:   # 현재 시간에 시작해야하는 과제가 존재하는 경우
-            stack.append(info[time])    # stack에 그 과제 추가
+    while queue:
+        rest_picks, cur_pick, cur_pick_rest, cur, cur_fatigue = queue.popleft()
 
-        time += 1    # 1분씩 시간이 흐름
+        # 광물을 모두 캤거나, 곡괭이를 모두 사용했으면 answer와 비교하여 갱신
+        if cur >= len(minerals) or (rest_picks == [0, 0, 0] and cur_pick_rest == 0):
+            answer = min(answer, cur_fatigue)
+            continue
+
+        # 현재까지의 피로도가 answer보다 크거나 같으면 pass
+        if cur_fatigue >= answer:
+            continue
+
+        mineral = names[minerals[cur]]
+
+        # 현재 사용중인 곡괭이를 더 사용할 수 있는 경우
+        if cur_pick_rest > 0:
+            next_rest_picks = [rest for rest in rest_picks]
+            queue.append(
+                (next_rest_picks, cur_pick, cur_pick_rest - 1, cur + 1, cur_fatigue + fatigues[cur_pick][mineral]))
+
+        # 새로운 곡괭이를 선택해야 하는 경우, 각 곡괭이에 대한 경우를 큐에 넣음
+        else:
+            for pick, rest in enumerate(rest_picks):
+                if rest == 0:
+                    continue
+
+                next_rest_picks = [rest for rest in rest_picks]
+                next_rest_picks[pick] -= 1
+                queue.append((next_rest_picks, pick, 4, cur + 1, cur_fatigue + fatigues[pick][mineral]))
 
     return answer
