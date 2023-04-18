5540a01558c68f08180f919c065d04f36614a1a2
Back_end_Developer_SH <zx028906@naver.com>
2023-04-18 14:58:32 +0900

    1.배열.sorted(key=lambda, x:[첫번째 조건, 두번째 조건] 2 나머지들의 합 나머지 다시보기 3 ^= bitwise xor 연산자 answer ^= result

--- 2023_04_18/pro_2_TableHash.py
+++ 2023_04_18/pro_2_TableHash.py
@@ -1,6 +1,6 @@
 def solution(data, col, row_begin, row_end):
     answer = 0
-    #동일 값 일경우 data 기준으로 내림차순 -x[0]을 기억해야한다.
+    #동일 값 일경우 data 기준으로 내림차순 -x[0]을 기억해야한다!
     d=sorted(data,key=lambda x : (x[col-1],-x[0]))
     for i in range(row_begin,row_end+1):
         result=0
