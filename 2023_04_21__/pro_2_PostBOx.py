52c0dd14a8af3aa6dd743a816305f513377ef9c9
Back_end_Developer_SH <zx028906@naver.com>
2023-04-21 16:19:46 +0900

    문제에서 여분의 컨베이어는 필요 x순서를 뒤집어 놓은 거기때문에 stack를 만들필요없이 빼버리고 다시 바로 비교하면된다.

--- 2023_04_21__/pro_2_PostBOx.py
+++ 2023_04_21__/pro_2_PostBOx.py
@@ -1,27 +1,13 @@
 from collections import deque
+
 def solution(order):
-    answer = 1
-    l=len(order)
-    stack=deque()
+    answer=0
+    container=[]
     order=deque(order)
-    p=deque()
-    x=order.popleft()
-    for i in range(x+1,l+1):
-        p.append(i)
-    for i in range(1,x):
-        A=x-i
-        stack.append(A)
-    for j in order:
-        if j == stack[0]:
-            answer += 1
-            stack.popleft()
-        elif j >= p[0]:
-            A=j-p[0]
-            p.remove(p[0:A])
-            stack.insert(0,p[0:A])
-        elif j == p[0]:
-            answer += 1
-        else:
-            return answer
+    for i in range(1,len(order)+1):
+        container.append(i)
+        while container and order[0] == container[-1]:
+            answer+=1
+            order.popleft()
+            container.pop()
     return answer
-print(solution([5, 4, 3, 2, 1]))
