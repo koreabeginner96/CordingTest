3c31f0da4b7af7a8da30020f16154b0c81dcb028
Back_end_Developer_SH <zx028906@naver.com>
2023-04-25 15:45:13 +0900

    sort바로 적용해서 그문구부터 시작해 중복 방지  for j in d[i-1]:  bitwise 함수  answer ^= result

---
+++ 2023_04_25/pro_2_Tablehash.py
@@ -0,0 +1,12 @@
+def solution(data, col, row_begin, row_end):
+    answer = 0
+    #동일 값 일경우 data 기준으로 내림차순 -x[0]을 기억해야한다!
+    d=sorted(data,key=lambda x : (x[col-1],-x[0]))
+    for i in range(row_begin,row_end+1):
+        result=0
+        #나머지들의 합
+        for j in d[i-1]:
+            result += (j % i)
+            # bitwise xor 연산자 ^=
+        answer ^= result
+    return answer
