25652819fc78c1f2bd9584c289c48f8cd70a331b
Back_end_Developer_SH <zx028906@naver.com>
2023-04-26 18:04:42 +0900

    숫자를 몫과 나머지로 분리하면 풀 수 있다.

---
+++ 20230_04_26/pro_2_MagicElevator(R).py
@@ -0,0 +1,17 @@
+def solution(storey):
+    answer = 0
+    while storey:
+        r=storey%10
+        if r <=4:
+            answer+=r
+        elif r >=6:
+            answer+=(10-r)
+            storey+=10
+        else:
+            if (storey/10)%10<4:
+                answer+=r
+            else:
+                answer+=r
+                storey+=10
+        storey=storey//10
+    return answer
