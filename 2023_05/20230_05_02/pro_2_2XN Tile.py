cd78eaea6d16c08938cd2407b4a3be6776bebd68
Back_end_Developer_SH <zx028906@naver.com>
2023-05-02 15:59:51 +0900

    전형적인 dp이다. 경우의 수 제한사항 1000000007 나머지로 구한다.

---
+++ 2023_05/20230_05_02/pro_2_2XN Tile.py
@@ -0,0 +1,8 @@
+def solution(n):
+    answer = 0
+    dp = [0 for i in range(n)]
+    dp[1], dp[2] = 1, 2
+    for i in range(3, n+1):
+        dp[i] = (dp[i-1] + dp[i-2]) % 1000000007
+    answer = dp[n]
+    return answer
