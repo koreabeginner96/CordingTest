97b2bfc97a88b777ef701038c08385b5a69a2b76
Back_end_Developer_SH <zx028906@naver.com>
2023-05-10 18:59:33 +0900

    리스트는 sort로 정리해야한다.

---
+++ 2023_05/2023_05_10/pro_2_MinNumbers.py
@@ -0,0 +1,9 @@
+def solution(A:list,B:list):
+    answer=0
+    A.sort()
+    B.sort(reverse=True)
+
+    for i in range(len(A)):
+        answer+=(A[i]*B[i])
+    return answer
+print(solution([1,2],[3,4]))
